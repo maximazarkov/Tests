43f7698256647b98d70b901b0af4d17e9708e2b5
Maxim Azarkov <aza-maxim@yandex.ru>
2019-12-29 16:01:19 +0700

    реализация бизнес заявки и класса хранилища в памяти

---
+++ Test1/src/main/java/ru/tests/techtracker/Item.java
@@ -0,0 +1,130 @@
+package ru.tests.techtracker;
+
+import java.util.Objects;
+
+/**
+ * Класс Item описывает бизнесс модель заявки.
+ * @author Azarkov Maxim
+ * @version $id$ 29.12.2019
+ */
+ /* На данный момент укажем только id и имя заявки.
+ * Далее возможно потребуется расширение полей и методов.
+ */
+public class Item {
+    private String id;
+    private String name;
+    private String desc;
+    private long time;
+
+    /**
+     *конструктор. содает элемент типа Item - заявка.
+     *@param name - имя заявки.
+     *@param desc - описание заявки.
+     *@param time - время создания заявки.
+     *@since 0.1.
+     */
+    public Item(String name, String desc, long time) {
+        this.name = name;
+        this.desc = desc;
+        this.time = time;
+    }
+
+    /**
+     *getId - возвращает уникального ключа заявки.
+     *@return значение уникального ключа заявки
+     *@since 0.1
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     У*setId - принимает значение уникального ключа заявки. уникальный ключ заявки - id элемента массива Item. Генерируется с помощью hash-функции.
+     *@param id - значение уникального ключа заявки типа String.
+     *@since 0.1.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     *getId - возвращает названия заявки.
+     *@return значение названия заявки типа String.
+     *@since 0.1.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *setName - принимает название заявки.
+     *@param name - значение названия заявки типа String.
+     *@since 0.1
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.id)
+                && Objects.equals(name, item.name);
+    }
+
+    /**
+     *setDesc - принимает описание заявки.
+     *@param desc - значение значение описания заявки типа String.
+     *@since 0.1.
+     */
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    /**
+     *getDesc - возвращает описание заявки.
+     *@return значение описания заявки типа String.
+     *@since 0.1.
+     */
+    public String getDesc() {
+        return this.desc;
+    }
+
+    /**
+     *setTime - принимает время создания заявки.
+     *@param time - значение времени создания заявки типа long.
+     *@since 0.1.
+     */
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    /**
+     *getTime - возвращает время создания заявки.
+     *@return значение времени создания заявки типа long.
+     *@since 0.1.
+     */
+    public long getTime() {
+        return this.time;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id='" + id + '\''
+                + ", name='" + name + '\''
+                + '}';
+    }
+}
---
+++ Test1/src/test/java/ru/tests/techtracker/TrackerTest.java
@@ -0,0 +1,109 @@
+package ru.tests.techtracker;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+
+    /*
+     *  добавление заявок - public Item add(Item item);
+     *  редактирование заявок - public boolean replace(String id, Item item);
+     *  удаление заявок - public boolean delete(String id);
+     *  получение списка всех заявок - public Item[] findAll();
+     *  получение списка по имени - public Item[] findByName(String key);
+     *  получение заявки по id - public Item findById(String id);
+     */
+
+    Tracker tracker;
+
+    @Before
+    public void loadOutputBefore() {
+        tracker = new Tracker();
+    }
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Item item = new Item("установить программу", "Установка программы Х", System.currentTimeMillis());
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenReplaceNameThenReturnNewName() {
+        Item previous = new Item("установить программу", "Установка программы Х", System.currentTimeMillis());
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(previous);
+        // Создаем новую заявку.
+        Item next = new Item("удалить программу", "Удаление программы Х", System.currentTimeMillis());
+        // Обновляем заявку в трекере.
+        tracker.replace(previous.getId(), next);
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findById(previous.getId()).getName(), is("удалить программу"));
+    }
+
+    @Test
+    public void whenDelete2ndItemNameThenReturn1stAnd3rdItem() {
+        Item item = new Item("установить программу Х1", "установить программу Х1", System.currentTimeMillis());
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(item);
+        // Создаем еще пару заявок.
+        item = new Item("установить программу Х2", "tустановить программу Х2", System.currentTimeMillis());
+        tracker.add(item);
+        String idForDelete = item.getId();
+        item = new Item("установить программу Х3", "установить программу Х3", System.currentTimeMillis());
+        tracker.add(item);
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.delete(idForDelete), is(true));
+    }
+
+    @Test
+    public void whenFindAllThenAllItems() {
+        Item item = new Item("установить программу Х1", "установить программу Х1", System.currentTimeMillis());
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(item);
+        // Создаем еще пару заявок.
+        item = new Item("установить программу Х2", "tустановить программу Х2", System.currentTimeMillis());
+        tracker.add(item);
+        item = new Item("установить программу Х3", "установить программу Х3", System.currentTimeMillis());
+        tracker.add(item);
+        List<Item> result  = tracker.findAll();
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findAll(), is(result));
+    }
+
+    @Test
+    public void whenFindByNameThenReturn2Items() {
+        Item item = new Item("установить программу Х1", "установить программу Х1", System.currentTimeMillis());
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(item);
+        // Создаем еще пару заявок.
+        item = new Item("установить программу Х2", "tустановить программу Х2", System.currentTimeMillis());
+        tracker.add(item);
+        item = new Item("установить программу Х1", "установить дополнительный модуль к программе Х1", System.currentTimeMillis());
+        tracker.add(item);
+        ArrayList<Item> allItem  = tracker.findByName("установить программу Х1");
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findByName("установить программу Х1"), is(allItem));
+    }
+
+    @Test
+    public void whenFindByIdThenReturn2Items() {
+        Item item = new Item("установить программу Х1", "установить программу Х1", System.currentTimeMillis());
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(item);
+        // Создаем еще пару заявок.
+        item = new Item("установить программу Х2", "tустановить программу Х2", System.currentTimeMillis());
+        tracker.add(item);
+        String idForFindId = item.getId();
+        item = new Item("установить программу Х1", "установить дополнительный модуль к программе Х1", System.currentTimeMillis());
+        tracker.add(item);
+        Item result  = tracker.findById(idForFindId);
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findById(idForFindId), is(result));
+    }
+}
